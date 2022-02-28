ae33949d27e91aad7920a3c8cad79d9c367c38c0
Andrew Sytchev <berryknoedel@gmail.com>
2022-02-28 14:30:02 +0300

    Remove unnecessary checks

--- fetched_data_processing.py
+++ fetched_data_processing.py
@@ -25,10 +25,8 @@ def print_asciitable(language_popularity, title):
 
 def predict_salary(salary_from, salary_to):
     if salary_from and salary_to:
-        (salary_from+salary_to) / 2
-    elif salary_from is None and salary_to is None:
-        return None
-    elif salary_from in (None, 0):
-        return salary_to*0.8
-    elif salary_to in (None, 0):
+        return (salary_from+salary_to) / 2
+    elif salary_from:
         return salary_from*1.2
+    elif salary_to:
+        return salary_to*0.8
